38d479888addff860d8b830bdfbf9d72a06dd1c6
sodmaq <sodmaq@gmail.com>
2025-10-14 10:51:39 +0100

    state management

--- client/e-commerce-ui/src/app/layout.tsx
+++ client/e-commerce-ui/src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import { ToastContainer } from "react-toastify";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -35,6 +36,14 @@ export default function RootLayout({
           {children}
           <Footer />
         </div>
+        <ToastContainer
+          autoClose={3000}
+          hideProgressBar
+          closeOnClick
+          rtl
+          pauseOnHover
+          position="top-right"
+        />
       </body>
     </html>
   );
--- client/e-commerce-ui/src/components/ProductCard.tsx
+++ client/e-commerce-ui/src/components/ProductCard.tsx
@@ -1,15 +1,19 @@
 "use client";
+import useCartStore from "@/stores/cartStore";
 import { ProductType } from "@/types";
 import { ShoppingCart } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 import React, { useState } from "react";
+import { toast } from "react-toastify";
 
 function ProductCard({ product }: { product: ProductType }) {
   const [productTypes, setProductTypes] = useState({
     size: product.sizes[0],
     color: product.colors[0],
   });
+  const { addToCart } = useCartStore();
+
   const handleProductType = ({
     type,
     value,
@@ -21,6 +25,16 @@ function ProductCard({ product }: { product: ProductType }) {
       ...prev,
       [type]: value,
     }));
+  };
+
+  const handleAddToCart = () => {
+    addToCart({
+      ...product,
+      quantity: 1,
+      selectedSize: productTypes.size,
+      selectedColor: productTypes.color,
+    });
+    toast.success("Product added to cart");
   };
   return (
     <div className="shadow-lg rounded-lg overflow-hidden">
@@ -87,7 +101,7 @@ function ProductCard({ product }: { product: ProductType }) {
         <div className="flex items-center justify-between">
           <p className="font-medium">${product.price.toFixed(2)}</p>
           <button
-            // onClick={handleAddToCart}
+            onClick={handleAddToCart}
             className="ring-1 ring-gray-200 shadow-lg rounded-md px-2 py-1 text-sm cursor-pointer hover:text-white hover:bg-black transition-all duration-300 flex items-center gap-2"
           >
             <ShoppingCart className="w-4 h-4" />
